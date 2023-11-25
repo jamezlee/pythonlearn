5b3347c71f873000194c05102d9ce141b110fd58
james.lee <james.lee@sptel.com>
2023-11-25 10:39:04 +0800

    learning

--- Learning/loop/main.py
+++ Learning/loop/main.py
@@ -3,4 +3,13 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
   print(fruit)
   print(fruit + " Pie")
+
 print(fruits)
+
+for number in range(1, 11):
+  print(number)
+
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
