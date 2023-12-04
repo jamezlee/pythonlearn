c04a39bc698dcd5f061159ba34753d9172c1a3dd
james <jlkh82@gmail.com>
2023-12-04 20:17:12 +0800

    learning

---
+++ Learning/greeting/main.py
@@ -0,0 +1,19 @@
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+    
+greet_with("james lee","singapore")
+
+
+import math
+
+def paint_calc(height, width, cover):
+  num_cans = (height * width) / cover
+  round_up_cans = math.ceil(num_cans)
+  print(f"You'll need {round_up_cans} cans of paint.")
+  
+# 🚨 Don't change the code below 👇
+test_h = int(input()) # Height of wall (m)
+test_w = int(input()) # Width of wall (m)
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
